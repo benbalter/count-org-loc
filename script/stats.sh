7b7fbee5ce91d9b489c681e02a4b06bac3114a06
Anusha Tirumuru <atirumuru@zscaler.com>
2023-06-10 23:51:09 +0530

    adding shell

--- script/stats.sh
+++ script/stats.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-pwd
-ls -lrt tmp
+while read -r line; do
+  cloc_out=$(cloc $line | grep SUM)
+  IFS=' ' read -r array <<< "$cloc_out"
+  echo "Repository: $line \nFiles: ${array[1]} \nblank: ${array[2]} \nComment: ${array[3]} \nTotal LOC: ${array[4]}"
+done <../repositories.cfg
